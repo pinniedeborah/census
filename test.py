47baa0320850690b95b87f8b48b2712d54a5bd00
pinniedeborah <pinniedeborah@gmail.com>
2024-06-19 14:01:57 +0300

    More Practise

--- test.py
+++ test.py
@@ -1,1 +1,32 @@
-1+1
+# a = 1
+# b = 2
+# c=a+b
+# print(c)
+
+"""if 5 > 2:
+ print("Five is greater than two!")
+print(10>9)
+print(10==9)
+print(10<9)
+a=20
+b=300
+if (a>b):
+    print ("A is greater than B")
+elif( b>a):
+    print("B is greater than A")
+
+print(bool("Hello"))
+print(bool(15))
+x="hello"
+y=15
+print(bool(x))
+print(bool(y))"""
+thislist=["food","juice","soda"]
+print(thislist)
+print(len(thislist))
+print(type(thislist))
+print(thislist[-3:-1])
+for x in thislist:
+    print(x)
+for i in range (len(thislist)):
+    print(thislist[i])
